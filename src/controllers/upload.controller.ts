66713dc0982e9cd240c44fb30bf4b587fbb67423
Agustin <agustinfederico6@gmail.com>
2025-11-13 02:45:49 -0300

    ahora las imagenes se obtienen del back, data-sql actualizado

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -3,7 +3,7 @@ import { type Request, type Response } from 'express';
 export class UploadController {
     
   public uploadImage = async (req: Request, res: Response) => {
-
+    console.log('Archivo recibido:', req.file);
     if (!req.file) {
       return res.status(400).json({ message: 'No se subió ninguna imagen' });
     }
